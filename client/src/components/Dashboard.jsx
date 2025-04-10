770373fddc87b47b085bad3613df4f2927af941b
Ethan Silverthorne <emsilver987@gmail.com>
2025-04-09 21:42:38 -0400

    Clean up Dashboard

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -12,11 +12,10 @@ import React from "react";
 import defaultProfile from "../assets/defaultUserProfile.jpg";
 
 const user = {
-  name: "Tom Cook",
-  email: "tom@example.com",
-  imageUrl:
-    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-};
+  name: " ",
+  email: " ",
+  };
+  
 const navigation = [
   { name: "Dashboard", href: "#", current: true },
   { name: "Workouts", href: "#", current: false },
@@ -195,7 +194,7 @@ export default function Dashboard() {
         </header>
         <main>
           <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            {/* Your content */}
+            {}
           </div>
         </main>
       </div>
