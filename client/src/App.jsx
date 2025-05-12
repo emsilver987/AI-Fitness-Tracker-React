d95c723713ee6719706555641ec0fb3244cfbd71
Ethan Silverthorne <emsilver987@gmail.com>
2025-05-11 20:08:21 -0400

    recreate UI/UX, all logic shoved into dashboard temporarily

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import Login from './components/Login.jsx';
-import Dashboard from './components/Dashboard';
-import Workouts from './components/Workouts';
-import Tracker from './components/Tracker';
+import Dashboard from './components/dashboard/Dashboard';
 
 function App() {
   return (
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/workouts" element={<Workouts />} />
-        <Route path="/tracker" element={<Tracker />} />
       </Routes>
   );
 };
